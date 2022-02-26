a75bccae94f71f31947a252611f3c1940ed56198
Leonard Moses <qalexeon@Xavier.lan>
2022-02-26 01:24:53 -0500

    created models--artist, models--song(with FK), created admin superuser.

--- tunr/admin.py
+++ tunr/admin.py
@@ -1,3 +1,7 @@
+import imp
 from django.contrib import admin
 
 # Register your models here.
+from .models import Artist
+
+admin.site.register(Artist)
--- tunr/models.py
+++ tunr/models.py
@@ -1,3 +1,19 @@
+from pyexpat import model
+from statistics import mode
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+# tunr/models.py
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+    photo_url = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
