9dcdd493b22e2ad4c8d887e4d03175152abca200
Leonard Moses <qalexeon@Xavier.lan>
2022-02-27 00:37:17 -0500

    finished song model.

--- tunr/admin.py
+++ tunr/admin.py
@@ -1,7 +1,7 @@
-import imp
 from django.contrib import admin
 
 # Register your models here.
-from .models import Artist
+from .models import Artist, Song
 
 admin.site.register(Artist)
+admin.site.register(Song)
--- tunr/models.py
+++ tunr/models.py
@@ -17,3 +17,9 @@ class Artist(models.Model):
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length=100, default='no song title')
+    album = models.CharField(max_length=100, default='no album title')
+    preview_url = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.title
